cfd1d1f63de99ecc4e9b84597c5be72e3e5b5ef5
Sachin Jadhav <jadhavsachin174@gmail.com>
2020-07-13 12:05:25 +0530

    Updated the tiemr file indentation

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
-const Timer = (props) => {
-  function timeInFormat(time){
-    const mod = time % 60 
-    const rem = time / 60
-    return `${Math.round(rem)}:${mod} mins` 
+const Timer = props => {
+  function timeInFormat(time) {
+    const mod = time % 60;
+    const rem = time / 60;
+    return `${Math.round(rem)}:${mod} mins`;
   }
-  return (
-      <div className="timer-label">
-        Time: {timeInFormat(props.time)}
-      </div>
-  );
+  return <div className="timer-label">Time: {timeInFormat(props.time)}</div>;
 };
 
-export default Timer
+export default Timer;
