236560f062f03f59b9d2fd1978f7c9b125444bef
Sachin Jadhav <jadhavsachin174@gmail.com>
2020-07-13 12:03:59 +0530

    Add function to render the timer count in min:seconds format

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Timer = (props) => {
+  function timeInFormat(time){
+    const mod = time % 60 
+    const rem = time / 60
+    return `${Math.round(rem)}:${mod} mins` 
+  }
   return (
       <div className="timer-label">
-        Time: {props.time}
+        Time: {timeInFormat(props.time)}
       </div>
   );
 };
